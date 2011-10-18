ef6ae153e6de324facb8215fdf575e9c936d5c77
Grzegorz Slowikowski <gslowikowski@gmail.com>
2011-10-18 06:17:46 +0000

    Inproved deployment scripts.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@3901 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

--- scripts/deploy/deploy-play11-framework.bat
+++ scripts/deploy/deploy-play11-framework.bat
@@ -1,3 +1,6 @@
+call mvn clean install --file %PLAY_HOME%/parent/pom.xml
+call mvn clean install --file %PLAY_HOME%/modules/parent/pom.xml
+
 set GROUP_ID_PREFIX=com.google.code.maven-play-plugin.
 set GROUP_ID=%GROUP_ID_PREFIX%org.playframework
 set ARTIFACT_ID=play
@@ -10,5 +13,6 @@ set REPO_URL=https://maven-play-plugin.googlecode.com/svn/mavenrepo/releases
 
 call mvn clean package source:jar javadoc:jar --file %SRC_DIR%/pom-build-dist.xml
 call mvn deploy:deploy-file -Dfile=%SRC_DIR%/%ARTIFACT_ID%.jar -DgroupId=%GROUP_ID% -DartifactId=%ARTIFACT_ID% -Dpackaging=jar -Dversion=%VERSION% -DpomFile=%SRC_DIR%/pom-dist.xml -Dsources=%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-sources.jar -Djavadoc=%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-javadoc.jar -Dfiles=%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-framework.zip,%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-framework-min.zip -Dtypes=zip,zip -Dclassifiers=framework,framework-min -DrepositoryId=%REPO_ID% -Durl=dav:%REPO_URL% -e
+
 rem call mvn package --file %SRC_DIR%/pom-build-dist.xml
 rem call mvn deploy:deploy-file -Dfile=%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-framework-min.zip -DgroupId=%GROUP_ID% -DartifactId=%ARTIFACT_ID% -Dclassifier=framework-min -Dpackaging=zip -Dversion=%VERSION% -DgeneratePom=false -DrepositoryId=%REPO_ID% -Durl=dav:%REPO_URL% -e

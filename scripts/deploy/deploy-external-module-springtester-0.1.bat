8ad5de17ac462e094ea1ea033c6fd7b15dbb9877
Grzegorz Slowikowski <gslowikowski@gmail.com>
2012-02-14 13:50:49 +0000

    #26 - Installation and deployment scripts for Play! SpringTester Module v. 0.1.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@7040 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ scripts/deploy/deploy-external-module-springtester-0.1.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=springtester
+set VERSION=0.1
+
+call deploy-external-module-with-jar.bat
---
+++ scripts/install/install-external-module-springtester-0.1.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=springtester
+set VERSION=0.1
+
+call install-external-module-with-jar.bat

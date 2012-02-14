e864164a42343631abba6f61139c5e1e785b1d3e
Grzegorz Slowikowski <gslowikowski@gmail.com>
2012-02-14 20:56:58 +0000

    #29 - Installation and deployment scripts for Play! Constretto Module v. 0.1.1.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@7120 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ scripts/deploy/deploy-external-module-constretto-0.1.1.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=constretto
+set VERSION=0.1.1
+
+call deploy-external-module-with-jar.bat
---
+++ scripts/install/install-external-module-constretto-0.1.1.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=constretto
+set VERSION=0.1.1
+
+call install-external-module-with-jar.bat

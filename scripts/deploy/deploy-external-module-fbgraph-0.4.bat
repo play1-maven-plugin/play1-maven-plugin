477537515ea1f100ca55335eb4500c31aa333af5
Grzegorz Slowikowski <gslowikowski@gmail.com>
2012-02-27 07:57:08 +0000

    #42 - Installation and deployment scripts for Play! FBGraph Module v. 0.4.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@7531 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ scripts/deploy/deploy-external-module-fbgraph-0.4.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=fbgraph
+set VERSION=0.4
+
+call deploy-external-module-with-jar.bat
---
+++ scripts/install/install-external-module-fbgraph-0.4.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=fbgraph
+set VERSION=0.4
+
+call install-external-module-with-jar.bat

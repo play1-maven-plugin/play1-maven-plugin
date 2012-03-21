f989d8cb1c902460866832991255c43cc6442993
Grzegorz Slowikowski <gslowikowski@gmail.com>
2012-03-21 17:55:13 +0000

    #23 - Implement "play:clean" mojo.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/trunk/plugin@8365 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ play-maven-plugin/src/main/java/com/google/code/play/PlayCleanMojo.java
@@ -0,0 +1,168 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.google.code.play;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.FileUtils;
+
+/**
+ * Clean Play! temporary directories: - "tmp"
+ * 
+ * @author <a href="mailto:gslowikowski@gmail.com">Grzegorz Slowikowski</a>
+ * @goal clean
+ * @phase clean
+ */
+public class PlayCleanMojo
+    extends AbstractPlayMojo
+{
+    /**
+     * Default Play! id (profile).
+     * 
+     * @parameter expression="${play.id}" default-value=""
+     * @since 1.0.0
+     */
+    protected String playId;
+
+    /**
+     * Should all "cleanable" directories be deleted. If "true", overrides all "cleanXXX" property values.
+     * 
+     * @parameter expression="${play.cleanAll}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean cleanAll;
+
+    /**
+     * Should "lib" and "modules" directories be deleted.
+     * 
+     * @parameter expression="${play.cleanDependencies}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean cleanDependencies;
+
+    /**
+     * Should "logs" directory be deleted.
+     * 
+     * @parameter expression="${play.cleanLogs}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean cleanLogs;
+
+    /**
+     * Should "precompiled" directory be deleted.
+     * 
+     * @parameter expression="${play.cleanPrecompiled}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean cleanPrecompiled;
+
+    /**
+     * Should "test-result" directory be deleted.
+     * 
+     * @parameter expression="${play.cleanTestResult}" default-value="false"
+     * @since 1.0.0
+     */
+    private boolean cleanTestResult;
+
+    /**
+     * Skip cleaning.
+     * 
+     * @parameter expression="${play.cleanSkip}" default-value="false"
+     * @required
+     * @since 1.0.0
+     */
+    private boolean cleanSkip = false;
+
+    protected void internalExecute()
+        throws MojoExecutionException, MojoFailureException, IOException
+    {
+        if ( cleanSkip )
+        {
+            getLog().info( "Cleaning skipped" );
+            return;
+        }
+
+        File baseDir = project.getBasedir();
+
+        File pidFile = new File( baseDir, "server.pid" );
+        if ( pidFile.exists() )
+        {
+            throw new MojoExecutionException(
+                                              String.format( "Play! Server started (\"%s\" file found), cleaning not allowed",
+                                                             pidFile.getName() ) );
+        }
+
+        File confDir = new File( baseDir, "conf" );
+        File configurationFile = new File( confDir, "application.conf" );
+
+        ConfigurationParser configParser = new ConfigurationParser( configurationFile, playId );
+        configParser.parse();
+        String tmpDirName = configParser.getProperty( "play.tmp", "tmp" );
+
+        if ( "none".equals( tmpDirName ) )
+        {
+            getLog().info( "No tmp folder will be used (play.tmp is set to \"none\")" );
+        }
+        else
+        {
+            File tmpDir = new File( tmpDirName );
+            if ( !tmpDir.isAbsolute() )
+            {
+                tmpDir = new File( baseDir, tmpDir.getPath() );
+            }
+            deleteDirectory( tmpDir );
+        }
+
+        if ( cleanAll || cleanDependencies )
+        {
+            deleteDirectory( new File( baseDir, "lib" ) );
+            deleteDirectory( new File( baseDir, "modules" ) );
+        }
+
+        if ( cleanAll || cleanLogs )
+        {
+            deleteDirectory( new File( baseDir, "logs" ) );
+        }
+
+        if ( cleanAll || cleanPrecompiled )
+        {
+            deleteDirectory( new File( baseDir, "precompiled" ) );
+        }
+
+        if ( cleanAll || cleanTestResult )
+        {
+            deleteDirectory( new File( baseDir, "test-result" ) );
+        }
+    }
+
+    private void deleteDirectory( File directory )
+        throws IOException
+    {
+        if ( directory.exists() )
+        {
+            getLog().info( String.format( "Deleting directory %s", directory ) );
+            FileUtils.deleteDirectory( directory );
+        }
+    }
+
+}

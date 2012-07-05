162ba10c24ea87c361b29c66f971bf54944c0cf7
Grzegorz Slowikowski <gslowikowski@gmail.com>
2012-07-05 19:54:01 +0000

    #49 - Installation and deployment scripts for Play! Scala Module v. 0.9.1. - continuation of svn rev.8246
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@11031 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

--- scripts/deploy/deploy-external-module-scala-0.9.1.bat
+++ scripts/deploy/deploy-external-module-scala-0.9.1.bat
@@ -7,8 +7,13 @@ set GROUP_ID_PREFIX=com.google.code.maven-play-plugin.
 set GROUP_ID=%GROUP_ID_PREFIX%org.playframework.modules.%MODULE_NAME%
 set ARTIFACT_ID=play-%MODULE_NAME%-compiler
 
+set REPO_ID=com.google.code.maven-play-plugin
+set REPO_URL=https://maven-play-plugin.googlecode.com/svn/mavenrepo/releases
+@rem set REPO_ID=com.google.code.maven-play-plugin-snapshots
+@rem set REPO_URL=https://maven-play-plugin.googlecode.com/svn/mavenrepo/snapshots
+
 call mvn clean package source:jar --file %SRC_DIR%/compiler-pom.xml
-call mvn deploy:deploy -Dfile=%SRC_DIR%/lib/%ARTIFACT_ID%.jar -DgroupId=%GROUP_ID% -DartifactId=%ARTIFACT_ID% -Dpackaging=jar -Dversion=%VERSION% -DpomFile=%SRC_DIR%/compiler-pom-dist.xml -Dsources=%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-sources.jar -DrepositoryId=%REPO_ID% -Durl=dav:%REPO_URL% -e
+call mvn deploy:deploy-file -Dfile=%SRC_DIR%/lib/%ARTIFACT_ID%.jar -DgroupId=%GROUP_ID% -DartifactId=%ARTIFACT_ID% -Dpackaging=jar -Dversion=%VERSION% -DpomFile=%SRC_DIR%/compiler-pom-dist.xml -Dsources=%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-sources.jar -DrepositoryId=%REPO_ID% -Durl=dav:%REPO_URL% -e
 
 set ARTIFACT_ID=play-%MODULE_NAME%
 call deploy-external-module-with-jar.bat

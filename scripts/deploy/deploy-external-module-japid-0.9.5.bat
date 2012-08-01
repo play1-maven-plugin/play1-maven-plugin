7a930ec4ea5c0c7fdf8874822a3bb2f96bb709a4
Grzegorz Slowikowski <gslowikowski@gmail.com>
2012-08-01 07:32:38 +0000

    #109 - Installation and deployment scripts for Play! Japid Module v.0.9.5.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@11093 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ scripts/deploy/deploy-external-module-japid-0.9.5.bat
@@ -0,0 +1,22 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=japid
+set VERSION=0.9.5
+set SRC_DIR=%MODULES_HOME%/%MODULE_NAME%-%VERSION%
+
+set GROUP_ID_PREFIX=com.google.code.maven-play-plugin.
+set GROUP_ID=%GROUP_ID_PREFIX%com.github.branaway.japid
+set ARTIFACT_ID=%MODULE_NAME%
+
+set REPO_ID=com.google.code.maven-play-plugin
+set REPO_URL=https://maven-play-plugin.googlecode.com/svn/mavenrepo/releases
+@rem set REPO_ID=com.google.code.maven-play-plugin-snapshots
+@rem set REPO_URL=https://maven-play-plugin.googlecode.com/svn/mavenrepo/snapshots
+
+call mvn clean package source:jar javadoc:jar --file %SRC_DIR%/%ARTIFACT_ID%-pom.xml
+call mvn deploy:deploy-file -Dfile=%SRC_DIR%/lib/%ARTIFACT_ID%-%VERSION%.jar -DgroupId=%GROUP_ID% -DartifactId=%ARTIFACT_ID% -Dpackaging=jar -Dversion=%VERSION% -DpomFile=%SRC_DIR%/%ARTIFACT_ID%-pom-dist.xml -Dsources=%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-sources.jar -Djavadoc=%SRC_DIR%/target/%ARTIFACT_ID%-%VERSION%-javadoc.jar -DrepositoryId=%REPO_ID% -Durl=dav:%REPO_URL% -e
+
+set GROUP_ID=%GROUP_ID_PREFIX%org.playframework.modules.%MODULE_NAME%
+set ARTIFACT_ID=play-%MODULE_NAME%
+set JAR_FILE_NAME=%MODULE_NAME%play-%VERSION%
+call deploy-external-module-with-jar.bat
+

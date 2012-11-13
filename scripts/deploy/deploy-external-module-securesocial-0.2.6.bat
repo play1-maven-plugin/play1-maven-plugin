1b72f19bb803396438f2d9f609baaa92c05d225a
Grzegorz Slowikowski <gslowikowski@gmail.com>
2012-11-13 18:11:51 +0000

    #112 - Installation and deployment scripts for Play! SecureSocial Module v.0.2.6.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@11268 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ scripts/deploy/deploy-external-module-securesocial-0.2.6.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=securesocial
+set VERSION=0.2.6
+
+call deploy-external-module-without-jar.bat
---
+++ scripts/install/install-external-module-securesocial-0.2.6.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=securesocial
+set VERSION=0.2.6
+
+call install-external-module-without-jar.bat

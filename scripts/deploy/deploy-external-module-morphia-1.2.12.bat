92d0eac81af17ca7e1ce84ab106ff5f85ff0cab8
Grzegorz Slowikowski <gslowikowski@gmail.com>
2013-02-25 20:10:23 +0000

    #117 - Mavenize Play! Morphia Module v.1.2.12 - installation and deployment scripts.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@11494 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ scripts/deploy/deploy-external-module-morphia-1.2.12.bat
@@ -0,0 +1,5 @@
+call ..\set-external-modules-home.bat
+set MODULE_NAME=morphia
+set VERSION=1.2.12
+
+call deploy-external-module-with-jar.bat
---
+++ scripts/install/install-external-module-morphia-1.2.12.bat
@@ -0,0 +1,7 @@
+rem call install-morphia-1.00-20110403
+
+call ..\set-external-modules-home.bat
+set MODULE_NAME=morphia
+set VERSION=1.2.12
+
+call install-external-module-with-jar.bat

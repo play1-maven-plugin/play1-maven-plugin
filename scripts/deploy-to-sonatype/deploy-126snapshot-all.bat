bed6b3ab085bd25027a5143ccee19fe93d3ed145
Grzegorz Slowikowski <gslowikowski@gmail.com>
2013-06-11 13:58:33 +0000

    #114 - Mavenize Play! Framework v.1.2.6-SNAPSHOT - some installation and deployment scripts fixes.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@11623 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

--- scripts/deploy-to-sonatype/deploy-126snapshot-all.bat
+++ scripts/deploy-to-sonatype/deploy-126snapshot-all.bat
@@ -1,6 +1,13 @@
-call deploy-126snapshot-play
-call deploy-126snapshot-play-crud
-call deploy-126snapshot-play-docviewer
-call deploy-126snapshot-play-grizzly
-call deploy-126snapshot-play-secure
-call deploy-126snapshot-play-testrunner
+set VERSION=1.2.6-SNAPSHOT
+call ..\set-play-home-%VERSION%.bat
+
+call mvn clean install --file %PLAY_HOME%/parent/pom.xml
+call mvn clean install --file %PLAY_HOME%/modules/parent/pom.xml
+
+@rem call deploy-126snapshot-deps
+@rem call deploy-126snapshot-play
+@rem call deploy-126snapshot-play-crud
+@rem call deploy-126snapshot-play-docviewer
+@rem call deploy-126snapshot-play-grizzly
+@rem call deploy-126snapshot-play-secure
+@rem call deploy-126snapshot-play-testrunner
--- scripts/install/install-126snapshot-all.bat
+++ scripts/install/install-126snapshot-all.bat
@@ -1,4 +1,11 @@
-rem call install-126snapshot-deps
+@rem call install-126snapshot-deps
+
+set VERSION=1.2.6-SNAPSHOT
+call ..\set-play-home-%VERSION%.bat
+
+call mvn clean install --file %PLAY_HOME%/parent/pom.xml
+call mvn clean install --file %PLAY_HOME%/modules/parent/pom.xml
+
 call install-126snapshot-play
 call install-126snapshot-play-crud
 call install-126snapshot-play-docviewer

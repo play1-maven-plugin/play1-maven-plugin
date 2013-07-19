686d209be435cd3d00141be3d6969004f518ed79
Grzegorz Slowikowski <gslowikowski@gmail.com>
2013-07-19 10:44:16 +0000

    #114 - Mavenize Play! Framework v.1.2.6-SNAPSHOT - installation scripts improved.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@11699 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

--- scripts/install/install-126snapshot-all.bat
+++ scripts/install/install-126snapshot-all.bat
@@ -3,9 +3,6 @@
 set VERSION=1.2.6-SNAPSHOT
 call ..\set-play-home-%VERSION%.bat
 
-call mvn clean install --file %PLAY_HOME%/parent/pom.xml
-call mvn clean install --file %PLAY_HOME%/modules/parent/pom.xml
-
 call install-126snapshot-play
 call install-126snapshot-play-crud
 call install-126snapshot-play-docviewer

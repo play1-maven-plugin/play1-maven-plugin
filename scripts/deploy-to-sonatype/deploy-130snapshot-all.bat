1f2526f64b36dfaf20618b9d20cf02bba5c347ee
Grzegorz Slowikowski <gslowikowski@gmail.com>
2013-07-19 10:47:51 +0000

    #115 - Mavenize Play! Framework v.1.3.0-SNAPSHOT - Sonatype deployment scripts improved.
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@11702 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

--- scripts/deploy-to-sonatype/deploy-130snapshot-all.bat
+++ scripts/deploy-to-sonatype/deploy-130snapshot-all.bat
@@ -1,10 +1,5 @@
-@rem call deploy-130snapshot-deps
-
 set VERSION=1.3.0-SNAPSHOT
 call ..\set-play-home-%VERSION%.bat
-
-call mvn clean install --file %PLAY_HOME%/parent/pom.xml
-call mvn clean install --file %PLAY_HOME%/modules/parent/pom.xml
 
 @rem call deploy-130snapshot-deps
 @rem call deploy-130snapshot-play

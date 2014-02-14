4aaaac99743324134513633e09e8f9f8a7c50a21
Grzegorz Slowikowski <gslowikowski@gmail.com>
2014-02-14 10:34:40 +0000

    Deployment scripts for version 1.0.0-beta7 of "play-app-default-parent" and "play-module-default-parent".
    git-svn-id: http://maven-play-plugin.googlecode.com/svn/tools@11811 9c2ad861-7886-cb7c-dcd7-2b8d41cd5de0

---
+++ scripts/deploy-to-sonatype/deploy-play-module-default-parent-1.0.0-beta7.bat
@@ -0,0 +1,11 @@
+set GROUP_ID=com.google.code.maven-play-plugin
+set ARTIFACT_ID=play-module-default-parent
+set VERSION=1.0.0-beta7
+set SRC_DIR=../../../tags/play-default-parents-%VERSION%/%ARTIFACT_ID%
+
+set REPO_ID=sonatype-nexus-staging
+set REPO_URL=https://oss.sonatype.org/service/local/staging/deploy/maven2
+@rem set REPO_ID=sonatype-nexus-snapshots
+@rem set REPO_URL=https://oss.sonatype.org/content/repositories/snapshots
+
+call mvn gpg:sign-and-deploy-file -Durl=%REPO_URL% -DrepositoryId=%REPO_ID% -DgeneratePom=false -Dfile=%SRC_DIR%/pom.xml -DgroupId=%GROUP_ID% -DartifactId=%ARTIFACT_ID% -Dpackaging=pom -Dversion=%VERSION% -Dgpg.homedir=./.gpg
